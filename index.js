2557e8b6a9c0b9cc5d9cbc88491be61cecd3e8aa
Acker Apple <acker@virgindata.com>
2016-04-05 13:29:24 -0400

    corrected node based version

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 "use strict";
 var ack = require('./ack-x')
 
-ack.modules.error.definePath('error','./js/error')
-ack.modules.number.definePath('number','./js/number')
-ack.modules.string.definePath('string','./js/string')
-ack.modules.binary.definePath('binary','./js/binary')
-ack.modules.base64.definePath('base64','./js/base64')
-ack.modules.object.definePath('object','./js/object')
-ack.modules.method.definePath('method','./js/method')
-ack.modules.array.definePath('array','./js/array')
-ack.modules.queryObject.definePath('queryObject','./js/queryObject')
-ack.modules.week.definePath('week','./js/week')
-ack.modules.month.definePath('month','./js/month')
-ack.modules.year.definePath('year','./js/year')
-ack.modules.date.definePath('date','./js/date')
-ack.modules.function.definePath('function','./js/function')
+ack.modules.definePath('error','./js/error')
+ack.modules.definePath('number','./js/number')
+ack.modules.definePath('string','./js/string')
+ack.modules.definePath('binary','./js/binary')
+ack.modules.definePath('base64','./js/base64')
+ack.modules.definePath('object','./js/object')
+ack.modules.definePath('method','./js/method')
+ack.modules.definePath('array','./js/array')
+ack.modules.definePath('queryObject','./js/queryObject')
+ack.modules.definePath('week','./js/week')
+ack.modules.definePath('month','./js/month')
+ack.modules.definePath('year','./js/year')
+ack.modules.definePath('date','./js/date')
+ack.modules.definePath('function','./js/function')
 
 //ensure modules are loaded by a lazy require
 ack.modules.getModule = function(name,path){
-	if(this.$storage[name])return this.$storage[name]
-	this.$storage[name] = require(path)
-	return this.$storage[name]
+  if(this.$storage[name])return this.$storage[name]
+  this.$storage[name] = require(path)
+  return this.$storage[name]
 }
 
 module.exports = ack
